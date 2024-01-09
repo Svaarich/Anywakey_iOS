e5a247de17b2665a9fdae66bd45b2edc7b096f6d
Svarich <svaarich@gmail.com>
2024-01-09 01:01:45 -0500

    NoDevicesView UI changes
    - Add rotating gradient background
    - divide view into separate parts

--- LANWakeUp-IOS/Custom Views/NoDevicesView.swift
+++ LANWakeUp-IOS/Custom Views/NoDevicesView.swift
@@ -6,6 +6,7 @@ struct NoDevicesView: View {
     
     @Environment(\.colorScheme) var colorScheme
     @State private var animate: Bool = false
+    @State private var gradientAngle: Double = 0
     @Binding var isPresented: Bool
     
     let secondaryAccenColor = Color("SecondaryAccentColor")
@@ -13,42 +14,15 @@ struct NoDevicesView: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 10) {
-                Text("There are no devices")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Text("Are you a productive person and can't wait to start your coputer remotely?\nClick the button to add a device to the list!")
-                    .padding(.bottom, 20)
-                Button {
-//                    AddDeviceView(isPresented: $isPresented)
-                    withAnimation(.spring) {
-                        isPresented.toggle()
-                    }
-                } label: {
-                    HStack {
-                        Text("Add device")
-                        Image(systemName: "pc")
-                            .symbolRenderingMode(.hierarchical)
-                    }
-                        .foregroundStyle(.white)
-                        .font(.headline)
-                        .frame(height: 55)
-                        .frame(maxWidth: .infinity)
-                        .background(animate ? secondaryAccenColor : .blue)
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
-                        .padding(.horizontal, animate ? 30 : 50)
-                        .scaleEffect(animate ? 1.1 : 1.0)
-                        .offset(y: animate ? -7 : 0)
-                }
+                description
+                button
             }
-            
-            .multilineTextAlignment(.center)
             .padding(40)
-            .shadow(
-                color: animate ? secondaryAccenColor.opacity(colorScheme == .dark ? 0.3 : 0.7) : .blue.opacity(colorScheme == .dark ? 0.5 : 0.7),
-                radius: animate ? 30 : 10,
-                x: 0,
-                y: animate ? 50 : 30)
-            .onAppear(perform: addAnimation)
+            
+            .onAppear {
+                addAnimation()
+                
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
@@ -63,7 +37,64 @@ struct NoDevicesView: View {
             ) {
                 animate.toggle()
             }
+            withAnimation(
+                Animation
+                    .linear(duration: 2)
+                    .repeatForever(autoreverses: false)
+            ) {
+                gradientAngle = 360
+            }
         }
+    }
+    
+    private var description: some View {
+        Group {
+            Text("There are no devices")
+                .font(.title)
+                .fontWeight(.semibold)
+            Text("Are you a productive person and can't wait to start your coputer remotely?\nClick the button to add a device to the list!")
+                .padding(.bottom, 20)
+        }
+        .multilineTextAlignment(.center)
+    }
+    
+    private var button: some View {
+        Button {
+            withAnimation(.spring) {
+                isPresented.toggle()
+            }
+        } label: {
+            HStack {
+                Text("Add device")
+                Image(systemName: "pc")
+            }
+            .foregroundStyle(.primary)
+            .font(.headline)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+
+            // background
+            .background {
+                Rectangle()
+                    .fill(
+                        AngularGradient(colors: [secondaryAccenColor, .blue], center: .center, angle: .degrees(gradientAngle))
+                    )
+                    .blur(radius: 35)
+                    .saturation(colorScheme == .dark ? 2.0 : 1.5)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .padding(.horizontal, animate ? 30 : 50)
+            .scaleEffect(animate ? 1.1 : 1.0)
+            .offset(y: animate ? -7 : 0)
+            
+            // shadow
+            .shadow(
+                color: .blue.opacity(colorScheme == .dark ? 0.5 : 0.7),
+                radius: animate ? 30 : 10,
+                x: 0,
+                y: animate ? 40 : 30)
+        }
+        .buttonStyle(.plain)
     }
 }
 
