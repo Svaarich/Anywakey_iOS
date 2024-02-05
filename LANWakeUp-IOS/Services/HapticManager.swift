029ce07abaea37e0822dda91fd10d847f4f9b7dc
Svaarich <svaarich@gmail.com>
2024-02-05 16:21:04 -0500

    Add Haptic manager

---
+++ LANWakeUp-IOS/Services/HapticManager.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+class HapticManager {
+    private init() {}
+    
+    static let instance = HapticManager()
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
