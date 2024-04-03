ea31a25d7ef03c8d765867b0adfb14ec5718df14
Svarich <svaarich@gmail.com>
2024-04-03 19:53:15 +0300

    create device-JSON export func

--- LANWakeUp-IOS/Extensions/Device + Extension.swift
+++ LANWakeUp-IOS/Extensions/Device + Extension.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 extension Device {
     // Copy device info to clipboard
-    func copyToShare() {
-        let shareDevice: String = "Name: \(self.name)\nIP: \(self.BroadcastAddr)\nMAC: \(self.MAC)\nPort: \(Port)"
+    func exportJSON() {
+        let shareDevice: String = """
+                                {
+                                "name": "\(name)",
+                                "MAC": "\(MAC)",
+                                "BroadcastAddr": "\(BroadcastAddr)",
+                                "Port": "\(Port)"
+                                }
+                                """
         UIPasteboard.general.string = shareDevice
-        print(shareDevice)
     }
 }
