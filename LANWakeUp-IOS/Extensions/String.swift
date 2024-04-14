faa5196022965076d227330c1aab2d13fc704b3a
Svarich <svaarich@gmail.com>
2024-04-14 23:58:17 +0300

    String ext for validation
    Validation of address / port

--- LANWakeUp-IOS/Extensions/String.swift
+++ LANWakeUp-IOS/Extensions/String.swift
@@ -8,4 +8,32 @@ extension String {
         let size = self.size(withAttributes: fontAttributes)
         return size
     }
+    
+    // Broadcast address validator
+    func isValidAdress() -> Bool {
+        var sin = sockaddr_in()
+        var sin6 = sockaddr_in6()
+        
+        if self.withCString({ cstring in inet_pton(AF_INET6, cstring, &sin6.sin6_addr) }) == 1 {
+            // IPv6 peer
+            return true
+        } else if self.withCString({ cstring in inet_pton(AF_INET, cstring, &sin.sin_addr) }) == 1 {
+            // IPv4 peer
+            return true
+        }
+        return false
+    }
+    
+    // Port validator
+    func isValidPort() -> Bool {
+        if self.isEmpty {
+            return true
+        } else {
+            if let _ = UInt16(self) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
 }
