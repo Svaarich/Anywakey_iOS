7e50676d9677d3103c0b3fb8fbd237d0c76903c6
Svaarich <svaarich@gmail.com>
2024-04-17 15:10:09 +0300

    Copied notification view

---
+++ LANWakeUp-IOS/Core/Components/CopiedNotificationView.swift
@@ -0,0 +1,26 @@
+
+import SwiftUI
+
+struct CopiedNotificationView: View {
+    
+    private var frameSize: CGFloat {
+        UIScreen.main.bounds.width / 3
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "doc.on.doc")
+                .font(.largeTitle).padding(.bottom, 4)
+                .foregroundStyle(.blue)
+            Text("COPIED!")
+                .font(.headline)
+                .fontWeight(.semibold)
+                .foregroundStyle(.primary)
+        }
+        .frame(width: frameSize, height: frameSize)
+        .background {
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundStyle(.ultraThickMaterial)
+        }
+    }
+}
