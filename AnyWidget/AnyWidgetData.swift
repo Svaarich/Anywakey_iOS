920fb6047329a7009223ece6c2f936cf68353fa1
Svarich <svaarich@gmail.com>
2024-05-15 00:36:26 +0300

    improve logic for device placement

--- AnyWidget/AnyWidgetData.swift
+++ AnyWidget/AnyWidgetData.swift
@@ -9,23 +9,21 @@ class AnyWidgetData: ObservableObject {
     }
     
     func fetchPinnedDevices() {
+        deviceList = []
         if let userDefaults = UserDefaults(suiteName: "group.svarich.anywakey") {
             if let data = userDefaults.data(forKey: "devices") {
                 do {
                     let decoder = JSONDecoder()
                     let savedDevices = try decoder.decode([Device].self, from: data)
                     let pinnedDevices = savedDevices.filter( { $0.isPinned } )
-                    var devices: [Device] = []
-                    if !pinnedDevices.isEmpty {
-                        for index in 0..<pinnedDevices.count {
-                            if index < 3 {
-                                devices.append(pinnedDevices[index])
-                            } else {
-                                return
-                            }
+                    guard !pinnedDevices.isEmpty else { return }
+                    if pinnedDevices.count <= 3 {
+                        deviceList = pinnedDevices
+                    } else {
+                        for index in 0..<3 {
+                            deviceList.append(pinnedDevices[index])
                         }
                     }
-                    deviceList = devices
                 } catch {
                     print("Unable to Decode devices (\(error))")
                 }
