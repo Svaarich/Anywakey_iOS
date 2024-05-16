5cfcf3ae2b30eeb217cad2f1430ecaf5c709b64d
Svarich <svaarich@gmail.com>
2024-05-16 22:23:19 +0300

    App Info Button

---
+++ Anywakey_iOS/Core/AppInfo/AppInfoButton.swift
@@ -0,0 +1,44 @@
+
+import SwiftUI
+
+struct AppInfoButton: View {
+    
+    private let text: String
+    private let image: Image
+    private let color: Color
+    private let imageColor: Color
+    private let action: () -> Void
+    
+    init(text: String, image: Image, color: Color, imageColor: Color = .white, action: @escaping () -> Void) {
+        self.text = text
+        self.image = image
+        self.color = color
+        self.imageColor = imageColor
+        self.action = action
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            HStack {
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(imageColor)
+                    .padding(5)
+                    .frame(width: 30, height: 30)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundStyle(color)
+                    }
+                Text(text)
+                    .padding(.leading, 8)
+                Spacer()
+            }
+            .padding(.vertical, 0.1)
+            .tint(.primary)
+        }
+    }
+
+}
