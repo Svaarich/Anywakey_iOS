7c23d11adbd259910ad40e1068bc67dbd8181de1
Svaarich <svaarich@gmail.com>
2024-05-23 15:28:00 +0300

    clean code

--- AnyWidget/Core/AnyWidgetEntryView.swift
+++ AnyWidget/Core/AnyWidgetEntryView.swift
@@ -28,49 +28,6 @@ extension AnyWidgetEntryView {
     
     // MARK: PROPERTIES
     
-    // main view
-    private var deviceListView: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            HStack(spacing: 6) {
-                Text("Devices")
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.primary)
-                Image(systemName: "star.fill")
-                    .foregroundStyle(Color.custom.starColor)
-            }
-            Divider()
-                .padding(.vertical, 6)
-            GeometryReader { geo in
-                VStack {
-                    ForEach(entry.list) { device in
-                        HStack(spacing: 0) {
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(device.name)
-                                    .lineLimit(1)
-                                    .frame(width: .infinity)
-                            }
-                            Spacer(minLength: 4)
-                            Button(intent: BootButtonIntent(id: device.id)) {
-                                Circle()
-                                    .foregroundStyle(.blue)
-                                    .overlay {
-                                        Image(systemName: "power")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .foregroundStyle(.white)
-                                            .padding(4)
-                                    }
-                            }
-                            .buttonStyle(.plain)
-                        }
-                        .frame(height: geo.size.height / 3)
-                        .frame(maxWidth: .infinity)
-                    }
-                }
-            }
-        }
-    }
-    
     // no pinned view
     private var noDeviceView: some View {
         VStack(alignment: .leading, spacing: 4) {
