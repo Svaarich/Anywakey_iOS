1e6d0d14b3bb1ffafa5bdb6c33b8f8a21e946830
Svaarich <svaarich@gmail.com>
2024-05-23 15:27:39 +0300

    add colors to provider

--- AnyWidget/Core/Provider.swift
+++ AnyWidget/Core/Provider.swift
@@ -6,20 +6,41 @@ struct Provider: TimelineProvider {
     
     @ObservedObject var widgetData = AnyWidgetData()
     
+    let colors = [
+        Color.widget.green,
+        Color.widget.blue,
+        Color.widget.orange,
+        Color.widget.pink,
+        Color.widget.purpule,
+        Color.widget.yellow
+    ]
+    
     func placeholder(in context: Context) -> SimpleEntry {
+        widgetData.fetchColorIndecies()
         widgetData.fetchPinnedDevices()
-        return SimpleEntry(list: widgetData.deviceList)
+        return SimpleEntry(
+            color1: colors[widgetData.indexColor1],
+            color2: colors[widgetData.indexColor2],
+            list: widgetData.deviceList)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        widgetData.fetchColorIndecies()
         widgetData.fetchPinnedDevices()
-        completion(SimpleEntry(list: widgetData.deviceList))
+        completion(SimpleEntry(
+            color1: colors[widgetData.indexColor1],
+            color2: colors[widgetData.indexColor2],
+            list: widgetData.deviceList))
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
         var entries: [SimpleEntry] = []
+        widgetData.fetchColorIndecies()
         widgetData.fetchPinnedDevices()
-        entries.append(SimpleEntry(list: widgetData.deviceList))
+        entries.append(SimpleEntry(
+            color1: colors[widgetData.indexColor1],
+            color2: colors[widgetData.indexColor2],
+            list: widgetData.deviceList))
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
