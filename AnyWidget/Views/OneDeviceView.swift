11749e6d6434e721face20f99fce7a2b587793f8
Svarich <svaarich@gmail.com>
2024-05-23 10:13:25 +0300

    improve views

--- AnyWidget/Views/OneDeviceView.swift
+++ AnyWidget/Views/OneDeviceView.swift
@@ -3,14 +3,17 @@ import SwiftUI
 
 struct OneDeviceView: View {
     
+    let color: [Color]
+    
     let device: Device
     
-    init(devices: [Device]) {
+    init(devices: [Device], color: [Color]) {
         self.device = devices[0]
+        self.color = color
     }
     
     var body: some View {
-        WidgetTile(device: device, colors: Color.widget.green.color, deviceAmount: 1)
+        WidgetTile(device: device, colors: color, deviceAmount: 1)
             .padding(4)
     }
 }
