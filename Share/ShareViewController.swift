f32ab6306bd969a60b66161e69cc8268d2e15cf4
Svarich <svaarich@gmail.com>
2024-05-29 23:30:29 +0300

    Share extension

---
+++ Share/ShareViewController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+import Social
+
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
