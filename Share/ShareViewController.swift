2a0537ab699072b2621cb86dcd3d37fd87236bc9
Svarich <svaarich@gmail.com>
2024-05-30 20:00:53 +0300

    unpack file func

--- Share/ShareViewController.swift
+++ Share/ShareViewController.swift
@@ -6,5 +6,19 @@ class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+    private func extractDevices() {
+        guard devices.isEmpty else { return }
+        DispatchQueue.global(qos: .userInteractive).async {
+            for provider in itemProviders {
+                let _ = provider.loadDataRepresentation(for: .text) { data, error in
+                    let decoder = JSONDecoder()
+                    do {
+                        devices = try decoder.decode([Device].self, from: data!)
+                    } catch {
+                        print("Unable to read data. \(error)")
+                    }
+                }
+            }
+        }
     }
 }
