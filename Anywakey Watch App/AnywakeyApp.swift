2280f396eb98f10fe3fa4fe8bf4e1d87da5003f8
Svaarich <svaarich@gmail.com>
2024-06-08 18:03:40 +0300

    watch app

---
+++ Anywakey Watch App/AnywakeyApp.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+@main
+struct Anywakey_Watch_AppApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                HomeView()
+            }
+        }
+    }
+}
