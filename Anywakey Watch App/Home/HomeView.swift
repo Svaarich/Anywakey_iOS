3ff17cc0eebb35f4d75dc4e6a0d8b67fcbf77115
Svarich <svaarich@gmail.com>
2024-06-10 15:26:26 +0300

    watch homeveiw

--- Anywakey Watch App/Home/HomeView.swift
+++ Anywakey Watch App/Home/HomeView.swift
@@ -1,33 +1,48 @@
 
 import SwiftUI
+import WatchConnectivity
 
 struct HomeView: View {
     
-    @State private var allDevices: [Device] = []
+    @ObservedObject var dataService = WatchDS()
     
     var body: some View {
         ScrollView {
             VStack {
-                if allDevices.isEmpty {
+                if dataService.allDevices.isEmpty {
                     Text("Please add device in the iOS app.")
                         .font(.subheadline)
                         .foregroundStyle(.secondary)
                 } else {
-                    ForEach(allDevices) { device in
+                    ForEach(dataService.allDevices) { device in
                         Button {
-                            
+                            //send device to ios app
+                            if WCSession.default.isReachable {
+                                let message = ["boot" : device] as! [String : Device]
+                                WCSession.default.sendMessage(message, replyHandler: nil) { error in
+                                    print("Watch error sending message: \(error)")
+                                    print(device)
+                                }
+                            }
                         } label: {
                             HStack {
                                 Text(device.name)
+                                    .lineLimit(1)
                                 Spacer()
-                                Image(systemName: "star.fill")
+                                if device.isPinned {
+                                    Image(systemName: "star.fill")
+                                        .foregroundStyle(.yellow)
+                                }
                             }
                         }
                     }
                 }
             }
         }
-        .navigationTitle(allDevices.isEmpty ? "No devices 🥲" : "Devices")
+        .navigationTitle(dataService.allDevices.isEmpty ? "No devices 🥲" : "Devices")
+        .onAppear {
+//            dataService.fecthSavedDevices()
+        }
     }
 }
 
