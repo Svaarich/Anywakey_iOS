10ac97f8b771a86ac2ba0e5a1e44d8cc1a62099a
Svaarich <svaarich@gmail.com>
2024-06-14 01:16:44 +0300

    Menu description

--- Anywakey Watch App/Home/MenuView.swift
+++ Anywakey Watch App/Home/MenuView.swift
@@ -3,9 +3,14 @@ import SwiftUI
 
 struct MenuView: View {
     
+    @ObservedObject var dataService = WatchDS()
+    
+    @State private var rotationAngle = 0.0
+    
     var body: some View {
-        Text("To start PC just press button.")
-    }
+        VStack(alignment: .leading) {
+            Text("To start PC just press button.")
+            Spacer()
             Button {
                 withAnimation(.smooth) {
                     rotationAngle -= 360
