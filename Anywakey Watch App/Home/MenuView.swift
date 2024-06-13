8b2593a2ea5981d94e26310d4503b3f18a6b72ee
Svaarich <svaarich@gmail.com>
2024-06-14 01:14:50 +0300

    refresh button

--- Anywakey Watch App/Home/MenuView.swift
+++ Anywakey Watch App/Home/MenuView.swift
@@ -6,4 +6,24 @@ struct MenuView: View {
     var body: some View {
         Text("To start PC just press button.")
     }
+            Button {
+                withAnimation(.smooth) {
+                    rotationAngle -= 360
+                }
+            } label: {
+                HStack {
+                    Text("Update devices")
+                    Spacer()
+                    Image(systemName: "arrow.circlepath")
+                        .rotationEffect(.degrees(rotationAngle))
+                }
+            }
+            .buttonBorderShape(.roundedRectangle)
+        }
+        .padding(.horizontal, 4)
+    }
+}
+
+#Preview {
+    MenuView(dataService: WatchDS())
 }
