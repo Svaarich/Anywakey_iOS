39b5d615def972d92de73906615326781fba079f
Svaarich <svaarich@gmail.com>
2024-06-14 01:32:12 +0300

    Revert "Move properties to ext"
    This reverts commit f4a536ca1f0b2c5172cc5e7a7163863d09286ce0.

--- Anywakey Watch App/Home/HomeView.swift
+++ Anywakey Watch App/Home/HomeView.swift
@@ -1,10 +1,11 @@
 
 import SwiftUI
+import WatchConnectivity
 
 struct HomeView: View {
     
     @ObservedObject var dataService = WatchDS()
-    @State private var isPressed: Bool = false
+    
     @State var loading: Bool = true
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -12,86 +13,63 @@ struct HomeView: View {
     var body: some View {
         VStack {
             if loading && dataService.allDevices.isEmpty {
-                loadingView
+                VStack(alignment: .leading) {
+                    Text("Awaiting connection with iPhone")
+                    ProgressView()
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
             } else {
                 if dataService.allDevices.isEmpty {
-                    // empty list view
-                    noDevicesView
+                    VStack(alignment: .leading) {
+                        Text("Device list is empty.")
+                        Text("Please add device in the iOS app.")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
                 } else {
-                    // device list view
-                    list
+                    List {
+                        ForEach(dataService.allDevices) { device in
+                            Button {
+                                //send device to ios app
+                                dataService.sendMessage(device: device)
+                            } label: {
+                                HStack {
+                                    Text(device.name)
+                                        .lineLimit(1)
+                                    Spacer()
+                                    if device.isPinned {
+                                        Image(systemName: "star.fill")
+                                            .foregroundStyle(.yellow)
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
-        }
-        .navigationTitle {
-            Text("Devices")
-                .foregroundStyle(.green)
         }
         .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                NavigationLink {
-                    MenuView()
-                } label: {
-                    Image(systemName: "list.bullet")
-                }
-                
-            }
-        }
-        // hide toolbar
-        .toolbar(dataService.allDevices.isEmpty ? .hidden : .visible)
-        
-        // ask for devices
-        .onReceive(timer) { _ in
-            deviceRequest()
-        }
-    }
-    
-    
-}
+                ToolbarItem(placement: .topBarLeading) {
+                    NavigationLink {
+                        MenuView()
+                    } label: {
+                        Image(systemName: "list.bullet")
+                    }
 
-extension HomeView {
-    
-    // MARK: PROPERTIES
-    
-    // progress view
-    private var loadingView: some View {
-        VStack(alignment: .leading) {
-            Text("Awaiting connection with iPhone")
-            ProgressView()
-            Spacer()
+                }
         }
-        .frame(maxWidth: .infinity)
-    }
-    
-    // empty list view
-    private var noDevicesView: some View {
-        VStack(alignment: .leading) {
-            Text("Device list is empty.")
-            Text("Please add device in the iOS app.")
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-            Spacer()
-        }
-        .frame(maxWidth: .infinity)
-    }
-    
-    // devices list
-    private var list: some View {
-        List {
-            ForEach(dataService.allDevices) { device in
-                ButtonRow(device: device)
-            }
-        }
-        .listStyle(.carousel)
-    }
-    
-    
-    private func deviceRequest() {
-        if dataService.session.isReachable {
-            dataService.askForDevices()
-            timer.upstream.connect().cancel()
-            withAnimation(.spring) {
-                loading = false
+        .navigationTitle("Devices")
+        .toolbar(dataService.allDevices.isEmpty ? .hidden : .visible)
+        .onReceive(timer) { _ in
+            if dataService.session.isReachable {
+                dataService.askForDevices()
+                timer.upstream.connect().cancel()
+                withAnimation(.spring) {
+                    loading = false
+                }
             }
         }
     }
@@ -99,24 +77,6 @@ extension HomeView {
 
 #Preview {
     NavigationStack {
-        HomeView(
-            dataService: WatchDS(devices: [
-                .init(name: "Lalala",
-                      MAC: "11:11:11:11:11:!1",
-                      BroadcastAddr: "123.123.123.123",
-                      Port: "65001"),
-                .init(name: "Lalala",
-                      MAC: "11:11:11:11:11:!1",
-                      BroadcastAddr: "myhostlalalalarge name",
-                      Port: "65001"),
-                .init(name: "Lalala",
-                      MAC: "11:11:11:11:11:!1",
-                      BroadcastAddr: "1.1.1.1",
-                      Port: "65001"),
-                .init(name: "Lalala",
-                      MAC: "11:11:11:11:11:!1",
-                      BroadcastAddr: "1.1.1.1",
-                      Port: "65001"),]),
-            loading: false)
+        HomeView()
     }
 }
