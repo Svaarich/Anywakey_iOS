fd8603cd10a209120b3df884545184a1d30cadb5
Danylo <svaarich@gmail.com>
2024-07-10 17:49:47 +0300

    View extension

--- app/Anywakey_iOS/Extensions/View.swift
+++ app/Anywakey_iOS/Extensions/View.swift
@@ -6,4 +6,9 @@ extension View {
         let resign = #selector(UIResponder.resignFirstResponder)
         UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
     }
+    
+    // Using in Bot Configuration View
+    public func highlighted(_ textColor: Color, _ backgroundColor: Color) -> some View {
+        modifier(HighlightedText(textColor, backgroundColor))
+    }
 }
